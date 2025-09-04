814e3b77afffb13e9a1baff40411d04665ea88fa
Leo Bortoluzzi (via WeWeb) <leobbortoluzzi@hotmail.com>
2025-09-04 20:23:23 -0300

    v8 - jhbjk

---
+++ src/store/libraries/state.js
@@ -0,0 +1,7 @@
+export default () => ({
+     /* wwFront:start */
+    // eslint-disable-next-line no-undef, no-dupe-keys
+    classes: {"8329b8b6-9baf-4304-b10f-766e22cd57c2":{"id":"8329b8b6-9baf-4304-b10f-766e22cd57c2","_state":{"style":{"default":{"width":"100%","border":"1px solid var(--bca65006-3840-40b6-a2fb-2d97f0365cc9,#e0e0e0)","height":"auto","padding":"14px","aspectRatio":"unset","borderRadius":"10px"}}},"content":{"default":{"_ww-text_color":"var(--57a931a5-d03e-43f5-8d39-69f199425c5e,#212121)","placeholderColor":"var(--551f65d9-65f5-4a0d-b3ae-5f4d6a07bbf9,#616161)","_ww-text_ellipsis":false,"_ww-text_fontSize":"16px","_ww-text_fontWeight":400,"_ww-text_lineHeight":"100%"}},"subClasses":{}}},
+    /* wwFront:end */
+    components: {},
+});
---
+++ vite.config.js
@@ -0,0 +1,75 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import fs from 'fs';
+import handlebars from 'handlebars';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+const pages = {"estados":{"outputDir":"./estados","lang":"en","title":"","cacheVersion":8,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/estados/"},{"rel":"alternate","hreflang":"en","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/estados/"}]},"whatsapp":{"outputDir":"./whatsapp","lang":"en","title":"","cacheVersion":8,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\">","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/whatsapp/"},{"rel":"alternate","hreflang":"en","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/whatsapp/"}]},"estados/:param":{"outputDir":"./estados/:param","lang":"en","title":"","cacheVersion":8,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/estados/:param/"},{"rel":"alternate","hreflang":"en","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/estados/:param/"}]},"index":{"outputDir":"./","lang":"en","title":"","cacheVersion":8,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/"},{"rel":"alternate","hreflang":"en","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/"}]},"__99ce74c8-7dd6-403c-8e4c-285280d90f83__path__":{"outputDir":"./__99ce74c8-7dd6-403c-8e4c-285280d90f83__path__","lang":"en","title":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__title__","cacheVersion":8,"meta":[{"name":"title","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__title__"},{"name":"description","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__desc__"},{"name":"keywords","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__keywords__"},{"name":"image","content":"/__99ce74c8-7dd6-403c-8e4c-285280d90f83__image__"},{"itemprop":"name","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__title__"},{"itemprop":"description","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__desc__"},{"itemprop":"image","content":"/__99ce74c8-7dd6-403c-8e4c-285280d90f83__image__"},{"name":"twitter:card","content":"summary"},{"name":"twitter:title","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__socialTitle__"},{"name":"twitter:description","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__socialDesc__"},{"name":"twitter:image","content":"/__99ce74c8-7dd6-403c-8e4c-285280d90f83__image__"},{"property":"og:title","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__socialTitle__"},{"property":"og:description","content":"__99ce74c8-7dd6-403c-8e4c-285280d90f83__socialDesc__"},{"property":"og:image","content":"/__99ce74c8-7dd6-403c-8e4c-285280d90f83__image__"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/__99ce74c8-7dd6-403c-8e4c-285280d90f83__path__/"},{"rel":"alternate","hreflang":"en","href":"https://a3db8d63-1f53-4982-82cd-aa5e9a3f2f87.weweb-preview.io/__99ce74c8-7dd6-403c-8e4c-285280d90f83__path__/"}]}};
+
+// Read the main HTML template
+const template = fs.readFileSync(path.resolve(__dirname, 'template.html'), 'utf-8');
+const compiledTemplate = handlebars.compile(template);
+
+// Generate an HTML file for each page with its metadata
+Object.values(pages).forEach(pageConfig => {
+    // Compile the template with page metadata
+    const html = compiledTemplate({
+        title: pageConfig.title,
+        lang: pageConfig.lang,
+        meta: pageConfig.meta,
+        scripts: {
+            head: pageConfig.scripts.head,
+            body: pageConfig.scripts.body,
+        },
+        alternateLinks: pageConfig.alternateLinks,
+        cacheVersion: pageConfig.cacheVersion,
+        baseTag: pageConfig.baseTag,
+    });
+
+    // Save output html for each page
+    if (!fs.existsSync(pageConfig.outputDir)) {
+        fs.mkdirSync(pageConfig.outputDir, { recursive: true });
+    }
+    fs.writeFileSync(`${pageConfig.outputDir}/index.html`, html);
+});
+
+const rollupOptionsInput = {};
+for (const pageName in pages) {
+    rollupOptionsInput[pageName] = path.resolve(__dirname, pages[pageName].outputDir, 'index.html');
+}
+
+export default defineConfig(() => {
+    return {
+        plugins: [nodePolyfills({ include: ['events', 'stream', 'string_decoder'] }), vue()],
+        base: "/",
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                },
+            },
+            postcss: {
+                plugins: [autoprefixer],
+            },
+        },
+        build: {
+            chunkSizeWarningLimit: 10000,
+            rollupOptions: {
+                input: rollupOptionsInput,
+                onwarn: (entry, next) => {
+                    if (entry.loc?.file && /js$/.test(entry.loc.file) && /Use of eval in/.test(entry.message)) return;
+                    return next(entry);
+                },
+                maxParallelFileOps: 900,
+            },
+        },
+        logLevel: 'warn',
+    };
+});
